413610bbe392b0984a0c4554be75fb6275864970
Mint Gould <wispymouse@gmail.com>
2026-09-23 16:05:09 -0700

    Coordinate +, -, *, and / Tests

---
+++ Assets/Tests/CoordinateTests.cs
@@ -0,0 +1,106 @@
+namespace TicTacCOSTCO.Tests
+{
+    using NUnit.Framework;
+    using System.Collections.Generic;
+    using System.Linq;
+    using TicTacCOSTCO.DataStructures;
+    using UnityEngine;
+    using TicTacCOSTCO.DataStructures.Tools;
+
+    public class CoordinateTests
+    {
+        public class CoordinatePlusResult_DataSource_Object
+        {
+            public Coordinate A;
+            public Coordinate B;
+            public Coordinate Expected;
+
+            public CoordinatePlusResult_DataSource_Object(Coordinate a, Coordinate b, Coordinate expected)
+            {
+                this.A = a;
+                this.B = b;
+                this.Expected = expected;
+            }
+
+            public override string ToString()
+            {
+                return $"({A} + {B} = {Expected}";
+            }
+        }
+
+        public static CoordinatePlusResult_DataSource_Object[] CoordinatePlusResult_DataSource =
+        {
+            new CoordinatePlusResult_DataSource_Object(new Coordinate(1, 0), new Coordinate(1, 0), new Coordinate(2, 0)),
+            new CoordinatePlusResult_DataSource_Object(new Coordinate(1, 0), new Coordinate(1, -5), new Coordinate(2, -5)),
+            new CoordinatePlusResult_DataSource_Object(new Coordinate(0, 0), new Coordinate(0, 0), new Coordinate(0, 0)),
+            new CoordinatePlusResult_DataSource_Object(new Coordinate(3, 3), new Coordinate(-2, -3), new Coordinate(1, 0)),
+            new CoordinatePlusResult_DataSource_Object(new Coordinate(-3, -3), new Coordinate(-2, -3), new Coordinate(-5, -6)),
+            new CoordinatePlusResult_DataSource_Object(new Coordinate(1999, -50), new Coordinate(2000, 25), new Coordinate(3999, -25))
+        };
+
+        /// <summary>
+        /// I expect A + B = C, C - A = B, and C - B = A.
+        /// </summary>
+        [Test]
+        [TestCaseSource(nameof(CoordinatePlusResult_DataSource))]
+        public void PlusPlusChecks(CoordinatePlusResult_DataSource_Object plan)
+        {
+            Coordinate result = plan.A + plan.B;
+            Coordinate aComponent = result - plan.B;
+            Coordinate bComponent = result - plan.A;
+            Assert.AreEqual(plan.Expected, result, $"Expected specific coordinate result.");
+            Assert.AreEqual(plan.A, aComponent, $"Should minus back to original coordinate");
+            Assert.AreEqual(plan.B, bComponent, $"Should minus back to original coordinate");
+        }
+
+        public class CoordinateMultiplyResult_DataSource_Object
+        {
+            public Coordinate A;
+            public int Multiplier;
+            public Coordinate Expected;
+
+            public CoordinateMultiplyResult_DataSource_Object(Coordinate a, int multiplier, Coordinate expected)
+            {
+                this.A = a;
+                this.Multiplier = multiplier;
+                this.Expected = expected;
+            }
+
+            public override string ToString()
+            {
+                return $"({A} * {Multiplier} = {Expected}";
+            }
+        }
+
+        public static CoordinateMultiplyResult_DataSource_Object[] CoordinateMultiplyResult_DataSource =
+        {
+            new CoordinateMultiplyResult_DataSource_Object(new Coordinate(1, 0), 3, new Coordinate(3, 0)),
+            new CoordinateMultiplyResult_DataSource_Object(new Coordinate(1, 0), 4, new Coordinate(4, 0)),
+            new CoordinateMultiplyResult_DataSource_Object(new Coordinate(0, 0), 5, new Coordinate(0, 0)),
+            new CoordinateMultiplyResult_DataSource_Object(new Coordinate(3, 3), 1, new Coordinate(3, 3)),
+            new CoordinateMultiplyResult_DataSource_Object(new Coordinate(-3, -3), 10, new Coordinate(-30, -30)),
+            new CoordinateMultiplyResult_DataSource_Object(new Coordinate(-3, -3), -10, new Coordinate(30, 30)),
+            new CoordinateMultiplyResult_DataSource_Object(new Coordinate(1999, -50), 0, new Coordinate(0, 0))
+        };
+
+        /// <summary>
+        /// I expect that A * B = C.
+        /// If B is not 0, I expect C / B = A.
+        /// </summary>
+        /// <param name="plan"></param>
+        [Test]
+        [TestCaseSource(nameof(CoordinateMultiplyResult_DataSource))]
+        public void MultiplyCheck(CoordinateMultiplyResult_DataSource_Object plan)
+        {
+            Coordinate result = plan.A * plan.Multiplier;
+            Assert.AreEqual(plan.Expected, result, $"Expected specific coordinate result.");
+
+            if (plan.Multiplier != 0)
+            {
+                Coordinate back = result / plan.Multiplier;
+                Assert.AreEqual(plan.A, back, $"Expected to be able to divide back to original result");
+            }
+        }
+    }
+
+}
--- Assets/Tests/GridTests.cs
+++ Assets/Tests/GridTests.cs
@@ -1,35 +1,37 @@
-using NUnit.Framework;
-using System.Collections.Generic;
-using System.Linq;
-using TicTacCOSTCO.DataStructures;
-using UnityEngine;
-using TicTacCOSTCO.DataStructures.Tools;
-
-public class GridTests
+namespace TicTacCOSTCO.Tests
 {
-    public class PlacementCausesSolve_DataSource_Object
-    {
-        public List<Coordinate> Placements;
-        public int Width;
-        public int Height;
-        public int ExpectedSolutions;
+    using NUnit.Framework;
+    using System.Collections.Generic;
+    using System.Linq;
+    using TicTacCOSTCO.DataStructures;
+    using UnityEngine;
+    using TicTacCOSTCO.DataStructures.Tools;
 
-        public PlacementCausesSolve_DataSource_Object(List<Coordinate> placements, int width, int height, int expectedSolutions)
+    public class GridTests
+    {
+        public class PlacementCausesSolve_DataSource_Object
         {
-            this.Placements = placements;
-            this.Width = width;
-            this.Height = height;
-            this.ExpectedSolutions = expectedSolutions;
+            public List<Coordinate> Placements;
+            public int Width;
+            public int Height;
+            public int ExpectedSolutions;
+
+            public PlacementCausesSolve_DataSource_Object(List<Coordinate> placements, int width, int height, int expectedSolutions)
+            {
+                this.Placements = placements;
+                this.Width = width;
+                this.Height = height;
+                this.ExpectedSolutions = expectedSolutions;
+            }
+
+            public override string ToString()
+            {
+                return $"({string.Join(", ", Placements.Select(x => x.ToString()))}) ({this.Width}x{this.Height}) ({this.ExpectedSolutions} expected)";
+            }
         }
 
-        public override string ToString()
+        public static PlacementCausesSolve_DataSource_Object[] PlacementCausesSolve_DataSource =
         {
-            return $"({string.Join(", ", Placements.Select(x => x.ToString()))}) ({this.Width}x{this.Height}) ({this.ExpectedSolutions} expected)";
-        }
-    }
-
-    public static PlacementCausesSolve_DataSource_Object[] PlacementCausesSolve_DataSource =
-    {
         // Simple single test cases
         new PlacementCausesSolve_DataSource_Object(
             new List<Coordinate>() { Coordinate.right, Coordinate.right + Coordinate.right, Coordinate.zero, },
@@ -65,33 +67,35 @@ public class GridTests
             5, 5, 0),
     };
 
-    [Test]
-    [TestCaseSource(nameof(PlacementCausesSolve_DataSource))]
-    public void PlacingThirdWouldResultInRow(PlacementCausesSolve_DataSource_Object plan)
-    {
-        int lastIndex = plan.Placements.Count - 1;
-        GameState testState = new GameState(plan.Width, plan.Height, 1);
-
-        for (int ii = 0; ii < lastIndex; ii++)
+        [Test]
+        [TestCaseSource(nameof(PlacementCausesSolve_DataSource))]
+        public void PlacingThirdWouldResultInRow(PlacementCausesSolve_DataSource_Object plan)
         {
-            testState.SetSideOwnership(plan.Placements[ii], 0, out _);
-        }
+            int lastIndex = plan.Placements.Count - 1;
+            GameState testState = new GameState(plan.Width, plan.Height, 1);
 
-        List<CellsSolution> solutions = HypotheticalSolutionTool.GetSolutionsFromClaimingTile(testState, plan.Placements[lastIndex], 0);
-        Assert.AreEqual(plan.ExpectedSolutions, solutions.Count, $"Expecting a specific amount of solutions from placing the last tile");
-
-        testState.SetSideOwnership(plan.Placements[lastIndex], 0, out List<CellsSolution> newSolutions);
-        Assert.AreEqual(plan.ExpectedSolutions, newSolutions.Count, $"Especting a specific amount of solutions total");
-
-        if (plan.ExpectedSolutions > 0)
-        {
-            foreach (Coordinate position in plan.Placements)
+            for (int ii = 0; ii < lastIndex; ii++)
             {
-                if (!testState.AcceptedSolutions.TryGetValue(position, out List<CellsSolution> positionSolutions))
+                testState.SetSideOwnership(plan.Placements[ii], 0, out _);
+            }
+
+            List<CellsSolution> solutions = HypotheticalSolutionTool.GetSolutionsFromClaimingTile(testState, plan.Placements[lastIndex], 0);
+            Assert.AreEqual(plan.ExpectedSolutions, solutions.Count, $"Expecting a specific amount of solutions from placing the last tile");
+
+            testState.SetSideOwnership(plan.Placements[lastIndex], 0, out List<CellsSolution> newSolutions);
+            Assert.AreEqual(plan.ExpectedSolutions, newSolutions.Count, $"Especting a specific amount of solutions total");
+
+            if (plan.ExpectedSolutions > 0)
+            {
+                foreach (Coordinate position in plan.Placements)
                 {
-                    Assert.Fail($"There should be an accepted solution for {position}.");
+                    if (!testState.AcceptedSolutions.TryGetValue(position, out List<CellsSolution> positionSolutions))
+                    {
+                        Assert.Fail($"There should be an accepted solution for {position}.");
+                    }
                 }
             }
         }
     }
+
 }
